9d3f250c6d0232f6d7d6d93f4abfaf775eaa464a
MIGUEL PHILIPPI <miguelphilippi1@gmail.com>
2022-05-11 21:50:34 -0300

    aula11_05_2022

--- AulaAPI_IBGE_09_05_2022/geral.js
+++ AulaAPI_IBGE_09_05_2022/geral.js
@@ -2,19 +2,18 @@ $.ajax({
     type: "GET",
     dataType: "json",
     url: "https://servicodados.ibge.gov.br/api/v1/localidades/estados",
-    success: function (estado){
-        estado.sort(function(a,b){
+    success: function (data){
+        data.sort(function(a,b){
             return a.nome < b.nome ? -1 : a.nome > b.nome ? 1:0;
         })
 
         let option = "<option default>Selecione um estado</option>";
         
-        $.each(estado,function(i,value){
+        $.each(data,function(i,value){
             // console.log(i + " " + value.sigla +" "+ value.nome);
             option += "<option id="+value.id+" value="+value.sigla+">"+ value.nome +"</option>";
             
         })
-        estado.sort();
         $("#endereco").find("#estado").append(option);
     },
     error: function (){
@@ -30,5 +29,39 @@ function mudancaCidade(){
     //pegaria o 𝙫𝙖𝙡𝙪𝙚 do elemento
     // const id = $("#estado").val();
     const id = $("#estado").find(":selected").attr("id");
-    alert("Você selecionou o valor "+id);
+    // alert("Você selecionou o valor "+id);
+
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: "https://servicodados.ibge.gov.br/api/v1/localidades/estados/"+id+"/distritos",
+        success: function (data){
+            data.sort(function(a,b){
+                return a.nome < b.nome ? -1 : a.nome > b.nome ? 1:0;
+            })
+
+            // let option = "<option default>Selecione uma cidade</option>";
+            let option = "";
+            $.each(data,function(i,value){
+                option += `<option id="${value.id}" value = "${value.nome}">${value.nome}</option>`;
+            })
+            $("#endereco").find("#cidade").append(option);
+        },
+        error: function (data){
+            
+        },
+        beforeSend: function (){
+            $("#endereco").find("#cidade option").remove();
+            // $("#resultado").show();
+        }
+    })
+}
+function escolher(){
+    $("body").find("#resultado").show();
+    $("#resultado").find(".estado-atual").text($("#estado").find(":selected").text());
+    $("#resultado").find(".cidade-atual").text($("#cidade").find(":selected").text());
+}
+
+function fechar(){
+    $("#resultado").hide();
 }
